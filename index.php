5f69138cc6dc4c154db9982aec963dab74a5e6fd
anuragh2003 <anuraghjidige@gmail.com>
2025-05-06 16:07:44 +0530

    basic first php class answers

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+echo "Jidige Anuragh<br>"; //first answer 
+echo "Hello I am 'John Doe'<br>";//second answer
+echo "Hello I'am \"John Doe\"";
+ const num=7;//third answer 
+$p=1;
+$t=2;
+$r=2;
+$sp=$p*$t*$r/100;
+echo "<br>sp:".$sp;//fourth answer 
+// echo "Enter a number: ";
+// $number = trim(fgets(STDIN)); // Take input from user
+// $square = $number * $number;
+// echo "The square of $number is $square.\n";//fifth answer(doubt)
+$a=2;
+$b=1;
+$quo=$a/$b;
+echo "<br>q:".$quo; 
+$rem=$a%$b;
+echo "<br>r:".$rem; //seventh answer
+?>
